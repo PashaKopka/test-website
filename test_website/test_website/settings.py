a4d07df7326c1aa58c3037d89657cfb08366593e
PashaKopka <pashakopka216@gmail.com>
2021-04-16 14:59:05 +0300

    [new feature] change database engine from sqlite to postgresql

--- test_website/test_website/settings.py
+++ test_website/test_website/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'test_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'weather_db',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
